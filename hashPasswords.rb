a7c894b33894a9a70ca4eaa8c179c75b39763ba9
MexpressoBean <kingk102@gmail.com>
2022-03-04 13:30:31 -0800

    building out hashing method

--- hashPasswords.rb
+++ hashPasswords.rb
@@ -1,3 +1,8 @@
+# 1. get text from file
+# 2. hash the text with different hash algos
+# 3. create objects with all neccessary info (Q, ans, wordlist, encryption)
+# 4. add rows and use objects to fill in columns
+
 # Hashing methods used:
 # bcrypt, scrypt, sha-256, sha-512, md5
 
@@ -17,36 +22,46 @@ require 'scrypt'
 
 # methods
 def read_in_txt_from_wordlist(list, line_count)
-    lines = File.foreach(list).first(line_count)
+    lines = File.foreach(list).first(line_count) # return array of strings
+end
+
+def create_password_objects(list, line_count)
+    password_strings = read_in_txt_from_wordlist(list, line_count)
+    pass_objects = {}
+
+    password_strings.each_with_index do | element, index |
+        hashed_str = create_hash_md5(element.chomp)
+
+        puts "Hash: csukram#{index}:#{hashed_str[:hash]}, Answer: #{element.chomp}, List: #{list}, Encryption: #{hashed_str[:alg]}"
+    end
 end
 
 def create_hash_bcrypt(input)
-    hash = BCrypt::Password.create(input)
+    output = BCrypt::Password.create(input)
+    {hash: output, alg: "bcrypt"}
 end
 
 def create_hash_scrypt(input)
-    hash = SCrypt::Password.create(input)
+    output = SCrypt::Password.create(input)
 end
 
 def create_hash_sha256(input)
-    hash = OpenSSL::Digest::SHA256.hexdigest input
+    output = OpenSSL::Digest::SHA256.hexdigest input
 end
 
 def create_hash_sha512(input)
-    hash = OpenSSL::Digest::SHA512.hexdigest input
+    output = OpenSSL::Digest::SHA512.hexdigest input
 end
 
 def create_hash_md5(input)
-    hash = OpenSSL::Digest::MD5.hexdigest input
+    output = OpenSSL::Digest::MD5.hexdigest input
+    {hash: output, alg: "md5"}
 end
 
 
 # MAIN ******************************************************
-text = "kevin"
 
-array = read_in_txt_from_wordlist("john.txt", 10)
-
-puts array
+create_password_objects("john.txt", 10)
 
 # END MAIN **************************************************
 
