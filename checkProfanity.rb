4de61c79ec2d5e9a2fc6b439a3a63fb9de3fd531
MexpressoBean <kingk102@gmail.com>
2022-03-04 14:46:30 -0800

    added profanity finder and removed bad words

---
+++ checkProfanity.rb
@@ -0,0 +1,24 @@
+require 'bundler/inline'
+
+gemfile do
+  source 'https://rubygems.org'
+  gem 'swearjar'
+end
+
+require "swearjar"
+
+# ****************************************************
+def read_in_txt_from_wordlist(list, line_count)
+    sj = Swearjar.default
+    lines = File.foreach(list).first(line_count) # return array of strings
+
+    lines.select{|l| sj.profane?(l) == true}
+end
+
+
+badwords = read_in_txt_from_wordlist("john.txt", 2000)
+
+badwords.each do |w|
+    puts w
+end
+# ****************************************************
--- hashPasswords.rb
+++ hashPasswords.rb
@@ -1,11 +1,3 @@
-# 1. get text from file
-# 2. hash the text with different hash algos
-# 3. create objects with all neccessary info (Q, ans, wordlist, encryption)
-# 4. add rows and use objects to fill in columns
-
-# Hashing methods used:
-# bcrypt, scrypt, sha-256, sha-512, md5
-
 require 'bundler/inline'
 
 gemfile do
@@ -27,15 +19,39 @@ end
 
 def create_password_objects(list, line_count)
     password_strings = read_in_txt_from_wordlist(list, line_count)
-    pass_objects = {}
+    pass_objects =[]
 
     password_strings.each_with_index do | element, index |
-        hashed_str = create_hash_md5(element.chomp)
+        case index
+        when 0..300
+          hashed_str = create_hash_md5(element.chomp)
+          pass_objects << {question: "csukram#{index}:#{hashed_str[:hash]}", answer: "#{element.chomp}", list: "#{list}", encryption: "#{hashed_str[:alg]}"}
+        when 301..600
+          hashed_str = create_hash_sha256(element.chomp)
+          pass_objects << {question: "csukram#{index}:#{hashed_str[:hash]}", answer: "#{element.chomp}", list: "#{list}", encryption: "#{hashed_str[:alg]}"}
+        when 601..900
+          hashed_str = create_hash_sha512(element.chomp)
+          pass_objects << {question: "csukram#{index}:#{hashed_str[:hash]}", answer: "#{element.chomp}", list: "#{list}", encryption: "#{hashed_str[:alg]}"}
+        when 901..1200
+          hashed_str = create_hash_bcrypt(element.chomp)
+          pass_objects << {question: "csukram#{index}:#{hashed_str[:hash]}", answer: "#{element.chomp}", list: "#{list}", encryption: "#{hashed_str[:alg]}"}
+        when 1201..1500
+          hashed_str = create_hash_scrypt(element.chomp)
+          pass_objects << {question: "csukram#{index}:#{hashed_str[:hash]}", answer: "#{element.chomp}", list: "#{list}", encryption: "#{hashed_str[:alg]}"}
+        else
+          puts 'err'
+        end
 
-        puts "Hash: csukram#{index}:#{hashed_str[:hash]}, Answer: #{element.chomp}, List: #{list}, Encryption: #{hashed_str[:alg]}"
+        puts "hashing: #{index}"
     end
+
+    puts "Hashing complete!"
+
+    pass_objects
 end
 
+# Hashing methods used:
+# bcrypt, scrypt, sha-256, sha-512, md5
 def create_hash_bcrypt(input)
     output = BCrypt::Password.create(input)
     {hash: output, alg: "bcrypt"}
@@ -43,14 +59,17 @@ end
 
 def create_hash_scrypt(input)
     output = SCrypt::Password.create(input)
+    {hash: output, alg: "scrypt"}
 end
 
 def create_hash_sha256(input)
     output = OpenSSL::Digest::SHA256.hexdigest input
+    {hash: output, alg: "sha256"}
 end
 
 def create_hash_sha512(input)
     output = OpenSSL::Digest::SHA512.hexdigest input
+    {hash: output, alg: "sha512"}
 end
 
 def create_hash_md5(input)
@@ -61,36 +80,42 @@ end
 
 # MAIN ******************************************************
 
-create_password_objects("john.txt", 10)
+row_data = create_password_objects("john.txt", 1500)
 
+# row_data objects includes:
+# question
+# answer
+# list
+# encryption
+# 
 # END MAIN **************************************************
 
-# # Excel 
-# p = Axlsx::Package.new
+# Excel 
+p = Axlsx::Package.new
 
-# # Required for use with numbers
-# p.use_shared_strings = true
+# Required for use with numbers
+p.use_shared_strings = true
 
-# p.workbook do |wb|
-#     # define your regular styles
-#     styles = wb.styles
-#     title = styles.add_style :sz => 15, :b => true, :u => true
-#     default = styles.add_style :border => Axlsx::STYLE_THIN_BORDER
-#     pascal_colors = { :bg_color => '567DCC', :fg_color => 'FFFF00' }
-#     pascal = styles.add_style pascal_colors.merge({ :border => Axlsx::STYLE_THIN_BORDER, :b => true })
-#     header = styles.add_style :bg_color => '00', :fg_color => 'FF', :b => true
+p.workbook do |wb|
+    # define your regular styles
+    styles = wb.styles
+    title = styles.add_style :sz => 15, :b => true, :u => true
+    default = styles.add_style :border => Axlsx::STYLE_THIN_BORDER
+    pascal_colors = { :bg_color => '567DCC', :fg_color => 'FFFF00' }
+    pascal = styles.add_style pascal_colors.merge({ :border => Axlsx::STYLE_THIN_BORDER, :b => true })
+    header = styles.add_style :bg_color => '00', :fg_color => 'FF', :b => true
 
 
-#     wb.add_worksheet(:name => 'CTC399 Password Hashes') do  |ws|
-#         ws.add_row ['CTC399 Password Hashes Questions - Kevin Ramirez (Student ID# 211141309)'], :style => title
-#         ws.add_row
-#         ws.add_row ['Questions', 'Answers', 'Wordlist', 'Encryption'], :style => header
+    wb.add_worksheet(:name => 'CTC399 Password Hashes') do  |ws|
+        ws.add_row ['CTC399 Password Hashes Questions - Kevin Ramirez (Student ID# 211141309)'], :style => title
+        ws.add_row
+        ws.add_row ['Questions', 'Answers', 'Wordlist', 'Encryption'], :style => header
 
-#         for a in 0..3 do 
-#             ws.add_row ['password hash', 'password', 'wordlist', 'sha-256']
-#         end
+        row_data.each do |p|
+            ws.add_row [p[:question], p[:answer], p[:list], p[:encryption]]
+        end
         
-#         ws.column_widths 96, 26, 31, 25 
-#     end
-# end
-# p.serialize 'test.xlsx'
+        ws.column_widths 96, 26, 15, 25 
+    end
+end
+p.serialize 'test.xlsx'
